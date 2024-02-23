ad11139ce2c56d09c1d96f99bc408bcd6c66770e
JBN303 <127194796+JBN303@users.noreply.github.com>
2024-02-23 16:03:06 +0530

    Add files via upload

---
+++ model/appointmentModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor' },
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+  patientName: String,
+  age: Number,
+  date: Date,
+  day: String,
+  purpose: String,
+  doctorName: String,
+  doctorLocation: String,
+  patientEmail: String, // Make sure these fields are defined in the schema
+  patientContactNo: String,
+  status: { type: String, default: 'pending' } // Add status field with default value 'active'
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
